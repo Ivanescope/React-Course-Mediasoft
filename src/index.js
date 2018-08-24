3454c613cc6bd823e4eab83b53d529c6414e1416
Ivanscope <vasiliy.golovin.2018@mail.ru>
2018-08-24 20:42:09 +0400

    First commit create app

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './style/style.css'
+import Counter  from './components/timer'
+
+
+
+ReactDOM.render(
+    <Counter />,
+    document.getElementById('root')
+);
